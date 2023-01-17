28df137afc25e3bde085b5e46d1457cee0f13bfd
interads <syarifuddin@interads.co.id>
2023-01-17 17:03:04 +0700

    Basic Sizing

--- app/src/main/java/co/id/jetpackcompose/MainActivity.kt
+++ app/src/main/java/co/id/jetpackcompose/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +17,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             Row(
                 modifier = Modifier
-                    .fillMaxSize()
+                    .width(300.dp)
+                    .height(200.dp)
                     .background(Color.Yellow),
                 horizontalArrangement = Arrangement.SpaceEvenly,
                 verticalAlignment = Alignment.CenterVertically
